90204600b2103c3b312c268be3057d4a3531f31f
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2022-11-29 10:18:00 +0530

    Doctors Section Made

---
+++ front-end/src/components/Doctors/Doctors.js
@@ -0,0 +1,130 @@
+import React, { useCallback, useState } from "react";
+import ForNotLoggedInUserDoctor from "../ForNotLoggedInUserDoctor";
+import PortalPopup from "../PortalPopup";
+import "./Doctors.css";
+
+function Doctors() {
+    const [isForNotLoggedInUserDoctorOpen, setForNotLoggedInUserDoctorOpen] =
+        useState(false);
+
+    const openForNotLoggedInUserDoctor = useCallback(() => {
+        setForNotLoggedInUserDoctorOpen(true);
+    }, []);
+
+    const closeForNotLoggedInUserDoctor = useCallback(() => {
+        setForNotLoggedInUserDoctorOpen(false);
+    }, []);
+
+    return (
+        <>
+            <div className="group-div146">
+                <div className="meet-our-doctors1">Meet Our Doctors</div>
+                <div className="group-div147">
+                    <div className="rectangle-div103" />
+                    <img
+                        className="ellipse-icon15"
+                        alt=""
+                        src="../ellipse-38@2x.png"
+                    />
+                    <div className="dr-guy-hawkins2">Dr. Guy Hawkins</div>
+                    <div className="ent-specialist-div2">ENT Specialist</div>
+                    <div className="mobile-number-98567412306">{`Mobile Number -> 9856741230`}</div>
+                    <button
+                        className="frame-button2"
+                        autoFocus
+                        onClick={openForNotLoggedInUserDoctor}
+                    >
+                        <div className="see-details-div6">See Details</div>
+                    </button>
+                </div>
+                <div className="group-div148">
+                    <div className="rectangle-div103" />
+                    <img
+                        className="ellipse-icon15"
+                        alt=""
+                        src="../ellipse-38@2x.png"
+                    />
+                    <div className="dr-guy-hawkins2">Dr. Guy Hawkins</div>
+                    <div className="ent-specialist-div2">ENT Specialist</div>
+                    <div className="mobile-number-98567412306">{`Mobile Number -> 9856741230`}</div>
+                    <button className="frame-button2" autoFocus>
+                        <div className="see-details-div6">See Details</div>
+                    </button>
+                </div>
+                <div className="group-div149">
+                    <div className="rectangle-div103" />
+                    <img
+                        className="ellipse-icon15"
+                        alt=""
+                        src="../ellipse-34@2x.png"
+                    />
+                    <div className="dr-wade-warren2">Dr. Wade Warren</div>
+                    <div className="obstetrics-gynaecology-speci2">{`Obstetrics & Gynaecology Specialist`}</div>
+                    <div className="mobile-number-98567412308">{`Mobile Number -> 9856741230`}</div>
+                    <button className="frame-button2" autoFocus>
+                        <div className="see-details-div6">See Details</div>
+                    </button>
+                </div>
+                <div className="group-div150">
+                    <div className="rectangle-div103" />
+                    <img
+                        className="ellipse-icon15"
+                        alt=""
+                        src="../ellipse-34@2x.png"
+                    />
+                    <div className="dr-wade-warren2">Dr. Wade Warren</div>
+                    <div className="obstetrics-gynaecology-speci2">{`Obstetrics & Gynaecology Specialist`}</div>
+                    <div className="mobile-number-98567412308">{`Mobile Number -> 9856741230`}</div>
+                    <button className="frame-button2" autoFocus>
+                        <div className="see-details-div6">See Details</div>
+                    </button>
+                </div>
+                <div className="group-div151">
+                    <div className="rectangle-div103" />
+                    <img
+                        className="ellipse-icon15"
+                        alt=""
+                        src="../ellipse-312@2x.png"
+                    />
+                    <div className="dr-jane-wilson2">Dr. Jane Wilson</div>
+                    <div className="orthopedics-specialist-div2">
+                        Orthopedics Specialist
+                    </div>
+                    <div className="mobile-number-98567412306">{`Mobile Number -> 9856741230`}</div>
+                    <button className="frame-button2" autoFocus>
+                        <div className="see-details-div6">See Details</div>
+                    </button>
+                </div>
+                <div className="group-div152">
+                    <div className="rectangle-div103" />
+                    <img
+                        className="ellipse-icon15"
+                        alt=""
+                        src="../ellipse-312@2x.png"
+                    />
+                    <div className="dr-jane-wilson2">Dr. Jane Wilson</div>
+                    <div className="orthopedics-specialist-div2">
+                        Orthopedics Specialist
+                    </div>
+                    <div className="mobile-number-98567412306">{`Mobile Number -> 9856741230`}</div>
+                    <button className="frame-button2" autoFocus>
+                        <div className="see-details-div6">See Details</div>
+                    </button>
+                </div>
+            </div>
+            {isForNotLoggedInUserDoctorOpen && (
+                <PortalPopup
+                    overlayColor="rgba(113, 113, 113, 0.6)"
+                    placement="Centered"
+                    onOutsideClick={closeForNotLoggedInUserDoctor}
+                >
+                    <ForNotLoggedInUserDoctor
+                        onClose={closeForNotLoggedInUserDoctor}
+                    />
+                </PortalPopup>
+            )}
+        </>
+    );
+}
+
+export default Doctors;
